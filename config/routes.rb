5268cdcd159b83fc98bcfac9b1bba28e3a511e6d
intissar chakour <chakourintissar08@hotmail.com>
2022-11-16 15:42:59 +0100

    yelp MVP done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    resources :restaurants do
+      resources :reviews, only: [:new, :create]
+    end
+    resources :reviews, only: [:destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
-end
